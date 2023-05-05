cf9c38a7ced0a56c2e1efe85dcda6aed69fd2aae
Adrian Bogdan <enu999@o2.pl>
2023-05-05 12:01:30 +0200

    styled.js Clock added

---
+++ src/Form/Clock/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledClock = styled.div`
+  font-size: 12px;
+  margin: 2% 5%;
+  display: flex;
+  flex-direction: row-reverse;
+  font-family: monospace;
+  color: white;
+  background-color: grey;
+`;
